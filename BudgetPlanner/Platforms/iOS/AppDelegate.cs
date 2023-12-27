dc52eccb7fb64611c0c55c468b215614c7703374
iafounta <lefteris.founta@outlook.com>
2023-12-28 00:17:43 +0100

    Add project files.

---
+++ BudgetPlanner/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BudgetPlanner
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
