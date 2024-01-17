70ff9dd6c713ed41b083ff67a325814436ef3b29
Niko Reichardt <niko.reichardt@stud.hslu.ch>
2024-01-17 22:34:09 +0100

    Correction on GlobalUsings

--- BudgetPlanner.Application/GlobalUsings.cs
+++ BudgetPlanner.Application/GlobalUsings.cs
@@ -3,7 +3,6 @@ global using Microsoft.Extensions.DependencyInjection;
 
 global using BudgetPlanner.Domain.Entities;
 global using BudgetPlanner.Domain.Results;
-global using BudgetPlanner.Domain.Interfaces;
 global using BudgetPlanner.Application.Interfaces;
 global using BudgetPlanner.Application.UseCases.Expenses;
 global using BudgetPlanner.Application.UseCases.Income;
--- BudgetPlanner.Infrastructure/GlobalUsings.cs
+++ BudgetPlanner.Infrastructure/GlobalUsings.cs
@@ -5,6 +5,7 @@ global using SQLite;
 
 global using BudgetPlanner.Infrastructure.Repositories;
 global using BudgetPlanner.Infrastructure.Interfaces;
+global using BudgetPlanner.Infrastructure.Entities;
 global using BudgetPlanner.Application.Interfaces;
 global using BudgetPlanner.Domain.Entities;
 global using BudgetPlanner.Domain.Results;
