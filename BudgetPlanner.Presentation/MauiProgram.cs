4cc02ca668506004b447ea4873e0e6dbbfb7bb00
iafounta <lefteris.founta@outlook.com>
2024-01-17 21:48:24 +0100

    fix merge bug

--- BudgetPlanner.Presentation/MauiProgram.cs
+++ BudgetPlanner.Presentation/MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿namespace BudgetPlanner.Presentation;
+﻿using BudgetPlanner.Infrastructure.Interfaces;
+using BudgetPlanner.Presentation.Service;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+
+namespace BudgetPlanner.Presentation;
 
 public static class MauiProgram
 {
@@ -7,7 +11,6 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .UseMicrocharts()
             .UseSkiaSharp()
             .ConfigureFonts(fonts =>
             {
@@ -33,9 +36,6 @@ public static class MauiProgram
         builder.Services.AddTransient<OverviewViewModel>();
         builder.Services.AddTransient<OverviewPage>();
 
-        builder.Services.AddApplicationServices();
-        builder.Services.AddInfrastructureServices(builder.Configuration);
-        builder.Services.AddSingleton<IDatabasePathProvider, DatabasePathProvider>();
 
         return builder.Build();
     }
